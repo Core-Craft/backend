2c38cf5016e5bdfde4cf50c86304327f2b6e63ba
amit <mramitdas99@gmail.com>
2023-09-30 21:07:15 +0530

    FIX: user api -> minor tweaks wrt previous commits

--- app/api/V1/endpoints/user.py
+++ app/api/V1/endpoints/user.py
@@ -1,68 +1,87 @@
 from fastapi import APIRouter, Request, Response, status, Depends, HTTPException, Query
-from app.models.user import User
-from app.serializers.user import UserSerializer
-from app.schemas.user import userEntity, usersEntity
-from app.db.base import DataBase
+
+from app.models.user import User as UserModel
+from app.schemas.user import BaseUser, UserIn, UserOut, UserSearch
 from .utils import hash_password, verify_password
-from dotenv import load_dotenv
 import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-db_url = os.environ.get('DB_URL')
-db_name = os.environ.get('DB_NAME')
-table_name = os.environ.get('TABLE_NAME')
+from typing import List
 
 user = APIRouter()
-db = DataBase(db_url=db_url)
 
 
-@user.get("/users/")
+@user.get("/users/", response_model=list[UserOut])
 async def get_users():
-    
-    """api covers below features
-    1. user search (single/multiple)
-    2. featch all users
+    """
+    Retrieve a list of users.
+
+    Returns a list of user data in the response.
+
+    This endpoint fetches user data from a data source and returns it as a list
+    of UserOut objects, which include user id, full name, and email.
+
+    Returns:
+        List[UserOut]: A list of user data.
     """
     
-    filter = {}
-    # func primarly written to get all users or specific user based on filter
-    # if filter is None:
-    return usersEntity(db.query(db_name=db_name, table_name=table_name, bulk=True))
+    user_instance = UserModel()
+    user_data = user_instance.get_all()
+
+    return user_data
 
 
-    # # 2nd condition (if user choose filter)
-    # db.query(db_name=db_name, table_name=table_name, bulk=True, filter=filter)
+# @user.post("/users/filter/", response_model=UserOut)
+# async def filter_users(filter: dict):
+#     """api covers below features
+#     1. user search (single/multiple)
+#     """
     
+#     user_instance = UserModel()  # Create an instance of the User model
+#     print(filter)
     
-@user.post("/register")
-async def register_user(user_data: User):
-    "api covers user registeration based on required details"
-    
-    user_dict = UserSerializer.from_request(user_data)
-    
-    user = db.query(db_name=db_name, table_name=table_name, filter={"_id"})
-    if user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail='Account already exist')
-        
-    # # Compare password and passwordConfirm
-    # if payload.password != payload.passwordConfirm:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_400_BAD_REQUEST, detail='Passwords do not match')
-    # #  Hash the password
-    # payload.password = hash_password(payload.password)
-    
+#     # try:
+#     #     filter_dict = filter.model_dump(exclude_unset=True)
+#     # except ValueError as e:
+#     #         raise HTTPException(status_code=400, detail=f"Invalid filter parameter: {e}")
 
-    db.upload(db_name=db_name, table_name=table_name, data=dict(user))
+#     # print(filter_dict)
+#     user_data = user_instance.get(filter=filter)
+#     UserOut.model_validate(user_data, from_attributes=True)
+#     print(user_data)
     
-    return {"status": "success"}
+    # return user_data
+    
+    
+@user.post("/user/register/")
+async def register_user(user_data: UserIn):
+    
+    # before entering data to db user search needs to be done using phone_no
+    # password hashing needs to be done
 
-@user.get("/login")
-async def login(user: User):
-    "api covers user login part using jwt"
     
-    db.query(db_name=db_name, table_name=table_name, filter={})
+#     user = db.query(db_name=db_name, table_name=table_name, filter={"_id"})
+#     if user:
+#         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+#                             detail='Account already exist')
+
     
-    
+    user_instance = UserModel()
+
+    try:
+        user_dict = user_data.model_dump(exclude_unset=False)
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=f"Invalid user data: {e}")
+
+    try:
+        user_instance.save(data=user_dict)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to register user: {e}")
+
+    return {
+        "status": "success",
+        "message": "User registration successful",
+        "data": {
+            "id": user_dict["id"],
+            "full_name": user_dict["name"],
+            "phone_no": user_dict["phone"],
+            }
+        }
