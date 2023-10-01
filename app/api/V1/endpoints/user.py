01d7f56faa61a3fcf77a32f4a9e0dca81869aefa
amit <mramitdas99@gmail.com>
2023-10-01 23:38:24 +0530

    FEAT: user api -> added user detail & user filter api along with api docs

--- app/api/V1/endpoints/user.py
+++ app/api/V1/endpoints/user.py
@@ -1,12 +1,31 @@
-from fastapi import APIRouter, Request, Response, status, Depends, HTTPException, Query
-
-from app.models.user import User as UserModel
-from app.schemas.user import BaseUser, UserIn, UserOut, UserSearch
-from .utils import hash_password, verify_password
-import os
 from typing import List
 
+from fastapi import APIRouter, HTTPException
+
+from app.models.user import User as UserModel
+from app.schemas.user import UserIn, UserOut, UserSearch
+
 user = APIRouter()
+
+
+@user.get(
+    "/user/{user_uuid}", response_model=UserIn, response_model_exclude={"password"}
+)
+async def get_user(user_uuid: int):
+    """
+    Get user data by user_uuid.
+
+    Args:
+        user_uuid (int): The ID of the user to retrieve.
+
+    Returns:
+        UserIn: The user data excluding the 'password' field.
+    """
+
+    user_instance = UserModel()
+    user_data = user_instance.get(user_uuid=user_uuid)
+
+    return user_data
 
 
 @user.get("/users/", response_model=list[UserOut])
@@ -17,53 +36,54 @@ async def get_users():
     Returns a list of user data in the response.
 
     This endpoint fetches user data from a data source and returns it as a list
-    of UserOut objects, which include user id, full name, and email.
+    of UserOut objects, which include user user_uuid, full name, and email.
 
     Returns:
         List[UserOut]: A list of user data.
     """
-    
+
     user_instance = UserModel()
     user_data = user_instance.get_all()
 
     return user_data
 
 
-# @user.post("/users/filter/", response_model=UserOut)
-# async def filter_users(filter: dict):
-#     """api covers below features
-#     1. user search (single/multiple)
-#     """
-    
-#     user_instance = UserModel()  # Create an instance of the User model
-#     print(filter)
-    
-#     # try:
-#     #     filter_dict = filter.model_dump(exclude_unset=True)
-#     # except ValueError as e:
-#     #         raise HTTPException(status_code=400, detail=f"Invalid filter parameter: {e}")
+@user.post("/users/filter/", response_model=List[UserOut])
+async def filter_users(filter: UserSearch):
+    """
+    Filter users based on the provided filter criteria.
 
-#     # print(filter_dict)
-#     user_data = user_instance.get(filter=filter)
-#     UserOut.model_validate(user_data, from_attributes=True)
-#     print(user_data)
-    
-    # return user_data
-    
-    
+    Args:
+        filter (UserSearch): The filter criteria for user search.
+
+    Returns:
+        List[UserOut]: A list of user data matching the filter criteria.
+    """
+
+    user_instance = UserModel()  # Create an instance of the User model
+    try:
+        user_dict = filter.model_dump(exclude_unset=True)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Invalid filter parameter: {e}")
+
+    try:
+        user_data = user_instance.filter(filter=user_dict)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to register user: {e}")
+
+    return user_data
+
+
 @user.post("/user/register/")
 async def register_user(user_data: UserIn):
-    
     # before entering data to db user search needs to be done using phone_no
     # password hashing needs to be done
 
-    
-#     user = db.query(db_name=db_name, table_name=table_name, filter={"_id"})
-#     if user:
-#         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-#                             detail='Account already exist')
+    #     user = db.query(db_name=db_name, table_name=table_name, filter={"_id"})
+    #     if user:
+    #         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+    #                             detail='Account already exist')
 
-    
     user_instance = UserModel()
 
     try:
@@ -80,8 +100,21 @@ async def register_user(user_data: UserIn):
         "status": "success",
         "message": "User registration successful",
         "data": {
-            "id": user_dict["id"],
-            "full_name": user_dict["name"],
-            "phone_no": user_dict["phone"],
-            }
-        }
+            "id": user_dict["user_uuid"],
+            "full_name": user_dict["full_name"],
+            "phone_no": user_dict["phone_no"],
+        },
+    }
+
+
+# @user.patch("/user/update/")
+# async def register_user(user_data: UserIn):
+#     """
+#     sample json
+
+#     {
+#         "filter": {"key": "val"},
+#         "data": {"key": "val"}
+#     }
+
+#     """
