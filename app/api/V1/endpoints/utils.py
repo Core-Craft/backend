bbe4752c4b361c49d3ea0acde54484c41563b1bf
amit <mramitdas99@gmail.com>
2023-10-01 23:43:29 +0530

    DOCS & STYLE: utils -> added documentation & code formatting

--- app/api/V1/endpoints/utils.py
+++ app/api/V1/endpoints/utils.py
@@ -4,8 +4,33 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
+    """
+    Hashes a plaintext password using a secure cryptographic hash.
+
+    Args:
+        password (str): The plaintext password to be hashed.
+
+    Returns:
+        str: The hashed password as a string.
+
+    Note:
+        This function uses the bcrypt hashing scheme for password hashing.
+    """
     return pwd_context.hash(password)
 
 
 def verify_password(password: str, hashed_password: str):
+    """
+    Verifies a plaintext password against a hashed password.
+
+    Args:
+        password (str): The plaintext password to be verified.
+        hashed_password (str): The previously hashed password to compare against.
+
+    Returns:
+        bool: True if the plaintext password matches the hashed password, False otherwise.
+
+    Note:
+        This function uses the bcrypt hashing scheme for password verification.
+    """
     return pwd_context.verify(password, hashed_password)
