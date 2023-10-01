cf3696c990ca6b8eab88111eb1017ca550b98415
amit <mramitdas99@gmail.com>
2023-10-01 23:47:42 +0530

    DOCS & STYLE: utils -> added documentation & code formatting

--- app/schemas/utils.py
+++ app/schemas/utils.py
@@ -1,7 +1,25 @@
 from datetime import datetime
-from pydantic import BaseModel
 import pytz
+from pydantic import BaseModel
+
 
 class TimestampMixin(BaseModel):
-    created_at: datetime = datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d || %H:%M:%S:%f")
-    updated_at: datetime = datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d || %H:%M:%S:%f")
+    """
+    A Pydantic mixin class to provide timestamp fields for created and updated times.
+
+    This mixin class extends Pydantic's `BaseModel` to include timestamp fields for both 'created_at' and 'updated_at' times. The timestamps are automatically generated in the 'Asia/Kolkata' timezone when an instance of a model that inherits from this mixin is created.
+
+    Attributes:
+        created_at (datetime): The timestamp indicating the creation time of the object.
+        updated_at (datetime): The timestamp indicating the last update time of the object.
+
+    Note:
+        - The timestamps are generated in the 'Asia/Kolkata' timezone.
+        - The format for the timestamps is '%Y-%m-%d || %H:%M:%S:%f'.
+    """
+    created_at: datetime = datetime.now(pytz.timezone("Asia/Kolkata")).strftime(
+        "%Y-%m-%d || %H:%M:%S:%f"
+    )
+    updated_at: datetime = datetime.now(pytz.timezone("Asia/Kolkata")).strftime(
+        "%Y-%m-%d || %H:%M:%S:%f"
+    )
