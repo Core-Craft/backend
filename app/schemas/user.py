050c8e8d9e843e1a6ec2e80c7590cb09bc84e4d3
amit <mramitdas99@gmail.com>
2023-10-01 23:49:13 +0530

    DOCS & STYLE: user schema -> added documentation & code formatting

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,45 +1,152 @@
-from pydantic import EmailStr, constr, validator
 import re
-from typing import Optional
+
+from bson import ObjectId
+from pydantic import BaseModel, EmailStr, constr, validator
+
 from .utils import TimestampMixin
-from pydantic import BaseModel
 
 
 class BaseUser(BaseModel):
-    id: Optional[int]
-    full_name: Optional[constr(
-        strip_whitespace=True,
-        max_length=50
-    )]
-    email: Optional[EmailStr]
-    phone_no: Optional[str]
-    aadhar_no: Optional[int]
-    user_type: Optional[constr(
-        strip_whitespace=True,
-        max_length=50
-    )]
-    
-    @validator("aadhar_no", pre=True, always=True)
-    def check_aadhar_no(cls, value):
-        if value is not None and not re.match(r"^\d{12}$", str(value)):
-            raise ValueError("Aadhar number must be exactly 12 digits long")
-        return value
-    
+    """
+    Represents the base user model with common attributes.
+
+    Attributes:
+        user_uuid (int): The unique identifier for the user.
+        full_name (str): The full name of the user, constrained to 50 characters with leading/trailing whitespaces stripped.
+        email (EmailStr, optional): The email address of the user, if provided.
+        phone_no (str): The phone number of the user.
+        aadhar_no (int, optional): The Aadhar number of the user, if provided.
+        user_role (str): The role or designation of the user, constrained to 50 characters with leading/trailing whitespaces stripped.
+
+    Config:
+        from_attributes (bool): Indicates whether attribute values should be populated from the corresponding class attributes when creating an instance. Defaults to True.
+    """
+
+    user_uuid: int
+    full_name: constr(strip_whitespace=True, max_length=50)
+    email: EmailStr | None = None
+    phone_no: str
+    aadhar_no: int | None = None
+    user_role: constr(strip_whitespace=True, max_length=50)
+
     class Config:
+        """
+        Configuration options for Pydantic models.
+
+        Attributes:
+            from_attributes (bool): Determines whether attribute values should be populated from class attributes when creating an instance of the model. If True, class attributes with the same name as fields in the model will be used to initialize those fields. Defaults to True, enabling attribute initialization from class attributes.
+        """
+
         from_attributes = True
 
-   
+
 class UserIn(TimestampMixin, BaseUser):
+    """
+    Represents a user input model with additional fields and validation.
+
+    Attributes:
+        _id (ObjectId): The unique identifier for the user.
+        password (str): The password associated with the user.
+        is_active (bool): Indicates whether the user account is active. Defaults to True.
+        is_superuser (bool): Indicates whether the user has superuser privileges. Defaults to False.
+
+    Validators:
+        check_aadhar_no (classmethod): A validator method that checks the Aadhar number (if provided) for its format. Raises a ValueError if the format is invalid.
+
+    Config:
+        from_attributes (bool): Indicates whether attribute values should be populated from the corresponding class attributes when creating an instance. Defaults to True.
+
+    Inherits from:
+        - TimestampMixin: A mixin class providing timestamp fields (e.g., created_at, updated_at).
+        - BaseUser: The base user model with common attributes.
+
+    Note:
+        The `check_aadhar_no` validator is applied to the `aadhar_no` field if a value is provided and ensures it is exactly 12 digits long.
+
+    """
+
+    _id: ObjectId
     password: str
     is_active: bool = True
     is_superuser: bool = False
-    
+
+    @validator("aadhar_no", pre=True, always=True)
+    def check_aadhar_no(self, value):
+        """
+        Validate the Aadhar number.
+
+        This validator method checks if the provided Aadhar number (if not None) is a valid 12-digit integer.
+
+        Args:
+            self: The current instance of the Pydantic model.
+            value: The value of the Aadhar number field to be validated.
+
+        Returns:
+            int or None: The validated Aadhar number if it is valid, or None if the input is None.
+
+        Raises:
+            ValueError: If the provided Aadhar number is not exactly 12 digits long or has a non-integer format.
+
+        Note:
+            This validator is intended for use with Pydantic models and is applied to the "aadhar_no" field.
+        """
+        if value is not None:
+            if not re.match(r"^\d{12}$", str(value)):
+                raise ValueError("Aadhar number must be exactly 12 digits long")
+        return value
+
     class Config:
+        """
+        Configuration options for Pydantic models.
+
+        Attributes:
+            from_attributes (bool): Determines whether attribute values should be populated from class attributes when creating an instance of the model. If True, class attributes with the same name as fields in the model will be used to initialize those fields. Defaults to True, enabling attribute initialization from class attributes.
+        """
+
         from_attributes = True
 
 
 class UserOut(BaseUser):
-    pass
+    """
+    Represents a user output model.
+
+    This class inherits attributes and behavior from the `BaseUser` class and is intended to be used for representing user data in output or response objects.
+
+    Inherits from:
+        - BaseUser: The base user model with common attributes.
+
+    Note:
+        This class does not introduce additional attributes or behavior beyond what is defined in the `BaseUser` class. It serves as a specialized version of `BaseUser` specifically designed for representing user data in response objects.
+    """
+
 
 class UserSearch(BaseUser):
-    pass
+    """
+    Represents a user search model with optional filter criteria.
+
+    This class inherits attributes and behavior from the `BaseUser` class but sets several fields to None, allowing them to be used as optional filter criteria when searching for users.
+
+    Inherits from:
+        - BaseUser: The base user model with common attributes.
+
+    Note:
+        - Fields that are set to None, such as `user_uuid`, `full_name`, `user_role`, and `phone_no`, can be used as optional filter criteria when performing user searches.
+        - When creating instances of this class, you can specify values for specific fields to filter user search results based on the provided criteria.
+    """
+
+    BaseUser.user_uuid = None
+    BaseUser.full_name = None
+    BaseUser.user_role = None
+    BaseUser.phone_no = None
+
+
+# class UserUpdate(BaseUser):
+
+#     filter = UserSearch
+#     data = BaseUser
+
+#     updated_at: datetime = datetime.now(pytz.timezone("Asia/Kolkata")).strftime("%Y-%m-%d || %H:%M:%S:%f")
+
+
+#     class Config:
+#         from_attributes = True
