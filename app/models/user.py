5be70ac95e8a456d86c435fc081b7f5efb367267
amit <mramitdas99@gmail.com>
2023-10-05 15:44:18 +0530

    FEAT: User model -> Refactor User class to inherit from Base
    Description:
    - Updated the User class to inherit from the newly created Base class, which provides generic database operations.
    - Removed duplicated code related to database interactions within the User class.
    - The User class now leverages the shared functionality provided by the Base class.
    - This change enhances code maintainability and reduces redundancy.

--- app/models/user.py
+++ app/models/user.py
@@ -1,132 +1,30 @@
-import os
-
-from dotenv import load_dotenv
-
-from app.db.base import DataBase
+from .base import Base
 
 
-class User:
+class User(Base):
     """
-    Represents a user data management utility.
+    Represents a class for managing user data.
 
-    This class provides methods for interacting with a user-related database table. It loads database configuration from environment variables and utilizes the `DataBase` class for performing common database operations such as saving, retrieving, filtering, updating, and deleting user data.
+    This class inherits from the `Base` class, which provides generic methods for interacting with a database table. The `User` class is specialized for managing user-related data and is configured to interact with a specific database table specified by the "USER_TABLE_NAME" environment variable.
 
     Attributes:
-        - __db_url (str): The database connection URL obtained from the environment variables.
-        - __db_name (str): The name of the database obtained from the environment variables.
-        - __table_name (str): The name of the user table obtained from the environment variables.
-        - __db (DataBase): An instance of the `DataBase` class for handling database operations.
+        None
 
     Methods:
-        - save(data: dict): Inserts user data into the database table.
-        - get(user_uuid: int): Retrieves user data by user UUID from the database table.
-        - get_all(): Retrieves all user data from the database table.
-        - filter(filter: dict): Retrieves user data based on a filter criteria from the database table.
-        - update(data: dict): Updates user data in the database table.
-        - delete(filter: dict): Deletes user data based on a filter criteria from the database table.
+        - __init__(): Initializes a `User` instance, inheriting the database connection and methods from the `Base` class.
 
     Note:
-        - The class initializes database-related attributes from environment variables loaded via `load_dotenv()`.
-        - It relies on the `DataBase` class for executing database operations.
+        - This class is designed for managing user data specifically and relies on the generic database operations provided by the `Base` class.
+        - The database table name is configured through the "USER_TABLE_NAME" environment variable.
     """
 
     def __init__(self):
-        # Load environment variables from .env file
-        load_dotenv()
-
-        self.__db_url = os.environ.get("DB_URL")
-        self.__db_name = os.environ.get("DB_NAME")
-        self.__table_name = os.environ.get("USER_TABLE_NAME")
-        self.__db = DataBase(db_url=self.__db_url)
-
-    def save(self, data: dict):
         """
-        Inserts user data into the database table.
+        Initialize a User instance for managing user-related data.
+
+        The database table name is determined by the "USER_TABLE_NAME" environment variable.
 
         Args:
-            data (dict): The user data to be saved.
-
-        Returns:
-            response: The response code from the database operation.
+            None
         """
-        return self.__db.upload(
-            db_name=self.__db_name, table_name=self.__table_name, data=data
-        )
-
-    def get(self, user_uuid: int):
-        """
-        Retrieves user data by user UUID from the database table.
-
-        Args:
-            user_uuid (int): The user UUID for identifying the user.
-
-        Returns:
-            response: The user data retrieved from the database.
-        """
-        return self.__db.query(
-            db_name=self.__db_name,
-            table_name=self.__table_name,
-            filter={"user_uuid": user_uuid}
-        )
-
-    def get_all(self):
-        """
-        Retrieves all user data from the database table.
-
-        Returns:
-            response: A list of all user data retrieved from the database.
-        """
-        return list(
-            self.__db.query(
-                db_name=self.__db_name, table_name=self.__table_name, bulk=True
-            )
-        )
-
-    def filter(self, filter: dict):
-        """
-        Retrieves user data based on a filter criteria from the database table.
-
-        Args:
-            filter (dict): The filter criteria for querying user data.
-
-        Returns:
-            response: A list of user data that matches the filter criteria.
-        """
-        return list(
-            self.__db.query(
-                db_name=self.__db_name,
-                table_name=self.__table_name,
-                filter=filter,
-                bulk=True
-            )
-        )
-
-    def update(self, data: dict):
-        """
-        Updates user data in the database table.
-
-        Args:
-            data (dict): The user data to be updated.
-
-        Returns:
-            response: The response code from the database operation.
-        """
-        return self.__db.update(
-            db_name=self.__db_name, table_name=self.__table_name, data=data
-        )
-
-    def delete(self, user_uuid: int):
-        """
-        Deletes user data based on a filter criteria from the database table.
-
-        Args:
-            filter (dict): The filter criteria for deleting user data.
-
-        Returns:
-            response: The response code from the database operation.
-        """
-        return self.__db.delete(
-            db_name=self.__db_name,
-            table_name=self.__table_name,
-            filter={"user_uuid": user_uuid}
-        )
+        super().__init__(table_name="USER_TABLE_NAME")
